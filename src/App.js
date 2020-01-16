7e37599465550162305123e6b6b9f8241417ee6f
Robin Beltran <robinbeltran@MacBook-Pro-de-Robin.local>
2020-01-15 22:09:50 -0600

    creando componentes y las rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Login from "./componets/auth/Login";
+import NuevaCuenta from "./componets/auth/NuevaCuenta";
+import Proyectos from "./componets/proyectos/Proyectos";
+
 function App() {
-  return <h1>MERN TASK</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
