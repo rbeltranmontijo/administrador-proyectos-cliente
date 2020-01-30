c0c8a093744045bd7ff7515bfd8ceb26893903d4
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-29 21:49:26 -0600

    agregando context para autentificacion

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import Proyectos from "./componets/proyectos/Proyectos";
 import ProyectoState from "./context/proyectos/proyectoState";
 import TareaState from "./context/tareas/tareaState";
 import AlertaState from "./context/alertas/alertaState";
+import AuthState from "./context/autenticacion/authState";
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
         <AlertaState>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </BrowserRouter>
+          <AuthState>
+            <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </BrowserRouter>
+          </AuthState>
         </AlertaState>
       </TareaState>
     </ProyectoState>
